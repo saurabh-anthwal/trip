0da19e236044e57b94cc2480049d34b250166c23
gauravanthwal <gauravanthwal7409@gmail.com>
2025-02-17 12:02:29 +0530

    nav remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layout/Navbar";
+// import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 
 const poppins = Poppins({
